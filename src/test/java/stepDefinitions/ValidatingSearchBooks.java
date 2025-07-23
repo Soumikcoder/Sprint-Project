0b5e1503b71a51ee969592d87fafea919b34a612
Abhinav-Pandey4610 <139861767+Abhinav-Pandey4610@users.noreply.github.com>
2025-07-23 20:51:47 +0530

    Adding ValidatingSearchBooks.java for step definition for the feature file

--- src/test/java/stepDefinitions/ValidatingSearchBooks.java
+++ src/test/java/stepDefinitions/ValidatingSearchBooks.java
@@ -3,6 +3,7 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.BasePage;
 
 public class ValidatingSearchBooks {
 	
