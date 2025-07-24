cb18d875fbe26e71b46598e76ef27f42cb911b08
abhinavappandey-byte <abhinav.ap.pandey@gmail.com>
2025-07-24 18:19:02 +0530

    Added SearchPage.java file

---
+++ src/test/java/pages/SearchPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SearchPage extends HomePage {
+
+	public SearchPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	@Override
+	public void openSearchTab() {
+		super.openSearchTab();
+	}
+
+}
