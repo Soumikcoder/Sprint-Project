634473e0da4f4e4a7fa5903856892395a7b24a91
Ankit Dhar Dwivedi <ankitdwi2472001@gmail.com>
2025-07-26 15:45:09 +0530

    Added the DataReader util file ,created the LibraryCard.xlsx excel file and changes has done in libraryCard Stepdefinition and libraryCard Page

--- src/test/java/pages/LibraryCardPage.java
+++ src/test/java/pages/LibraryCardPage.java
@@ -1,8 +1,11 @@
 package pages;
 
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class LibraryCardPage extends BasePage{
    
@@ -11,30 +14,95 @@ public class LibraryCardPage extends BasePage{
 		// TODO Auto-generated constructor stub
 	}
 	
+@FindBy(id = "first")
+private WebElement firstNameInput;
 
-//@FindBy(xpath="//input[@id='first']")
-//   WebElement firstInput;
-//@FindBy(xpath="//input[@id='last']")
-//WebElement firstInput;
-//@FindBy(xpath="//input[@id='age']")
-//WebElement firstInput;
-//@FindBy(xpath="//input[@id='email']");
-//WebElement firstInput;
-//@FindBy(xpath="//input[@id='phone']");
-//WebElement firstInput;
-//@FindBy(xpath="//input[@id='work_0']")
-//WebElement firstInput;
-//@FindBy(xpath="//input[@id='work_1']")
-//WebElement firstInput;
-//@FindBy(xpath="//select[@id='action']")
-//WebElement firstInput;
-//@FindBy(xpath="//input[@id='libraryCardSubmit']")
-//WebElement firstInput;
+@FindBy(id = "last")
+private WebElement lastNameInput;
+
+@FindBy(id = "age")
+private WebElement ageInput;
+
+@FindBy(id = "email")
+private WebElement emailInput;
+
+@FindBy(id = "phone")
+private WebElement phoneInput;
+
+@FindBy(xpath="//*[@id=\"work_0\"]")
+private WebElement Student;
+
+@FindBy(xpath="//*[@id=\"work_1\"]")
+ private WebElement Employee;
+@FindBy(id="company")
+private WebElement company;
+@FindBy(id="school")
+ private WebElement school;
+
+@FindBy(id = "action")
+private WebElement cardTypeDropdown;
+
+@FindBy(id = "libraryCardSubmit")
+private WebElement submitBtn;
+
+@FindBy(id = "idcard")  
+private WebElement successMessage;
+
+
+// Action Methods
+public void setFirstName(String firstName) {
+    firstNameInput.sendKeys(firstName);
+}
+
+public void setLastName(String lastName) {
+    lastNameInput.sendKeys(lastName);
+}
+
+public void setAge(String age) {
+    ageInput.sendKeys(age);
+}
+
+public void setEmail(String email) {
+    emailInput.sendKeys(email);
+}
+
+public void setPhone(String phone) {
+    phoneInput.sendKeys(phone);
+}
+
+public void setWork(String work,String OrgName) throws InterruptedException {
+         if(work.equalsIgnoreCase("student")) {
+        	 Student.click();
+        	 Thread.sleep(5000);
+        	 school.sendKeys(OrgName);
+         }
+         else 
+         {
+        	 Employee.click();
+        	 Thread.sleep(5000);
+        	 company.sendKeys(OrgName);
+         }
+}
+
+public void setCardType(String cardType) {
+    Select select = new Select(cardTypeDropdown);
+    if(cardType.equalsIgnoreCase("apply"))
+    select.selectByVisibleText("Apply New Card");
+}
+
+public void clickSubmit() {
+    submitBtn.click();
+}	
 	
 	
-	
-	
-	
+public boolean isFormSubmit() {
+    try {
+        return successMessage.isDisplayed();  
+       
+    } catch (NoSuchElementException e) {
+        return false;
+    }
+}
 	
 
 }
--- src/test/java/stepDefinitions/LibraryCardEntry.java
+++ src/test/java/stepDefinitions/LibraryCardEntry.java
@@ -6,46 +6,113 @@ import hooks.Hooks;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import junit.framework.*;
 import pages.HomePage;
+import pages.LibraryCardPage;
 import pages.MembershipPage;
+import utils.DataReader;
+
+import java.io.IOException;
+import java.util.*;
 
 public class LibraryCardEntry {
 	
 	   WebDriver driver;
 	   HomePage homePage;
+	   LibraryCardPage lcp;
 	   
-
-	public LibraryCardEntry(Hooks hooks) {
+  List<HashMap<String,String>>datamap;
+	
+  public LibraryCardEntry(Hooks hooks) {
 		this.driver = hooks.getDriver();
 		this.homePage = hooks.getHomepage();
+		
 	}
 
 @Given("Navigate to the url library card entry")
 public void navigate_to_the_url_library_card_entry() {
-    // Write code here that turns the phrase above into concrete actions
-   // throw new io.cucumber.java.PendingException();
+          homePage.openLibraryCardTab();
+          lcp = new LibraryCardPage(driver); 
 	  
 }
 
-@When("Enter the all the valid data")
-public void enter_the_all_the_valid_data() {
-    // Write code here that turns the phrase above into concrete actions
-   // throw new io.cucumber.java.PendingException();
-	
-	
+@Then("Enter the all the valid data and verify it {string}")
+public void enter_the_all_the_valid_data(String rows) throws InterruptedException {
+	try {
+		datamap=DataReader.data("C:\\Users\\91920\\git\\Sprint-Project\\src\\test\\resources\\LibraryCardData.xlsx", "Sheet1");
+	} 
+    catch (IOException e) 
+    {
+		e.printStackTrace();
+	}
+
+    int index=Integer.parseInt(rows)-1;
+    String first= datamap.get(index).get("first");
+    String last= datamap.get(index).get("last");
+    String age= datamap.get(index).get("age");
+    String email= datamap.get(index).get("email");
+    String phone= datamap.get(index).get("phone");
+    String work= datamap.get(index).get("work");
+    String orgName=datamap.get(index).get("orgName");
+    
+    String cardType= datamap.get(index).get("cardType");
+    String exp_res= datamap.get(index).get("res");
+    
+
+   
+    lcp.setFirstName(first);
+    lcp.setLastName(last);
+    lcp.setAge(age);
+    lcp.setEmail(email);
+    lcp.setPhone(phone);
+    lcp.setWork(work,orgName);
+   
+    lcp.setCardType(cardType);
+
+    lcp.clickSubmit();
+   
+    try
+    {
+        boolean targetpage=lcp.isFormSubmit();
+        System.out.println("target page: "+ targetpage);
+        if(exp_res.equalsIgnoreCase("Valid"))
+        {
+            if(targetpage==true)
+            {
+                
+               
+                Assert.assertTrue(true);
+            }
+            else
+            {
+                Assert.assertTrue(false);
+            }
+        }
+
+        if(exp_res.equalsIgnoreCase("Invalid"))
+        {
+            if(targetpage==true)
+            {
+                
+                Assert.assertTrue(false);
+            }
+            else
+            {
+                Assert.assertTrue(true);
+            }
+        }
+
+
+    }
+    catch(Exception e)
+    {
+
+        Assert.assertTrue(false);
+    }
+
+    
 }
 
-@When("Click on Submit button to submit the details")
-public void click_on_submit_button_to_submit_the_details() {
-    // Write code here that turns the phrase above into concrete actions
-   // throw new io.cucumber.java.PendingException();
-}
-
-@Then("Verify that the details are submitted succcesfully")
-public void verify_that_the_details_are_submitted_succcesfully() {
-    // Write code here that turns the phrase above into concrete actions
-   // throw new io.cucumber.java.PendingException();
-}
 
 	
 	
