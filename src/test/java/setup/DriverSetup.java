d2036dbc21fc4f2e3acfc6391b7794351a9e7436
Soumik <91443670+Soumikcoder@users.noreply.github.com>
2025-07-28 21:24:45 +0530

    added headless feature

--- src/test/java/setup/DriverSetup.java
+++ src/test/java/setup/DriverSetup.java
@@ -2,20 +2,29 @@ package setup;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 //TODO: Different Browser using prop file
 //TODO: 
 public class DriverSetup {
 	public static WebDriver getDriver() {
 		WebDriver driver=null;
-			String browser=AppPropProvider.get("browser");
+		String browser=AppPropProvider.get("browser");
+		boolean isHeadless=AppPropProvider.get("headless").equals("true");
 			switch(browser) {
 				case "firefox":
-					driver=new FirefoxDriver();
+					FirefoxOptions fo=new FirefoxOptions();
+					if(isHeadless)
+					fo.addArguments("--headless");
+					driver=new FirefoxDriver(fo);
 					break;
 				case "chrome":
-					driver=new ChromeDriver();
+					ChromeOptions co=new ChromeOptions();
+					if(isHeadless)
+					co.addArguments("--headless=new");
+					driver=new ChromeDriver(co);
 					break;
 				case "edge":
 					driver=new EdgeDriver();
