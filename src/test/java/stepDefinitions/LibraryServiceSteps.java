552db62faed198e9ebc9b1ab014615db25c85e6e
Jayantika122 <debjayantika@gmail.com>
2025-07-29 20:33:07 +0530

    Upadated Services

--- src/test/java/stepDefinitions/LibraryServiceSteps.java
+++ src/test/java/stepDefinitions/LibraryServiceSteps.java
@@ -1,107 +1,4 @@
-/*package stepDefinitions;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-
-import org.openqa.selenium.WebDriver;
-import hooks.Hooks;
-import io.cucumber.java.en.*;
-import junit.framework.Assert;
-import pages.LibraryServicePage;
-import utils.DataReader;
-
-public class LibraryServiceSteps {
-
-    WebDriver driver;
-    LibraryServicePage servicePage;
-    List<HashMap<String, String>> dataMap;
-    HashMap<String, String> testData;
-
-    public LibraryServiceSteps(Hooks hooks) {
-        this.driver = hooks.getDriver();
-        this.servicePage = new LibraryServicePage(driver);
-    }
-
-    @Given("the user navigates to the Library Services form")
-    public void navigate_to_form() {
-        servicePage.openLibraryServicesPage();  // Define this in your page class
-    }
-
-    @When("the user selects {string} option")
-    public void select_option(String option) {
-        (servicePage).selectOption(option);
-    }
-
-    @When("enters email {string}")
-    public void enter_email(String email) {
-        servicePage.enterEmail(email);
-    }
-
-    @When("enters query {string}")
-    public void enter_query(String query) {
-        servicePage.enterQuery(query);
-    }
-
-    @When("clicks Submit")
-    public void clicks_submit() {
-        servicePage.clickSubmit();
-    }
-
-    @Then("verify the result is {string}")
-    public void verify_the_result(String expectedResult) {
-        boolean isSuccess = (servicePage).isSubmissionSuccessful(expectedResult);
-        if (expectedResult.equalsIgnoreCase("success")) {
-            Assert.assertTrue("Expected success but got failure", isSuccess);
-        } else {
-            Assert.assertFalse("Expected failure but got success", isSuccess);
-        }
-    }
-
-    @Then("the message should be {string}")
-    public void message_should_be(String expectedMessage ,String actualMessage ) {
-         actualMessage = servicePage.getResultMessage();
-        Assert.assertTrue("Expected message to contain: " + expectedMessage, actualMessage.contains(expectedMessage));
-    }
-
-    // Optional: A step to use row_index from Excel (data-driven)
-    @Then("fill the Library form with data from row {string}")
-    public void fill_library_form_with_excel(String rowIndex) {
-        try {
-            String excelPath = System.getProperty("user.dir") + File.separator + "src" +
-                    File.separator + "test" + File.separator + "resources" + File.separator + "LibraryServiceForm.xlsx";
-            dataMap = DataReader.data(excelPath, "EmailTests");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        int index = Integer.parseInt(rowIndex) - 1;
-        testData = dataMap.get(index);
-
-        servicePage.selectOption(testData.get("option"));
-        servicePage.enterEmail(testData.get("email"));
-        servicePage.enterQuery(testData.get("query"));
-        servicePage.clickSubmit();
-    }
-
-    @Then("verify result and message from the same row")
-    public void verify_result_and_message_from_excel(String actualMessage) {
-        boolean actualResult = (servicePage).isSubmissionSuccessful(null);
-        String expectedResult = testData.get("expected_result");
-        String expectedMessage = testData.get("message");
-        actualMessage = servicePage.getResultMessage();
-
-        if (expectedResult.equalsIgnoreCase("success")) {
-            Assert.assertTrue("Expected success but got failure", actualResult);
-        } else {
-            Assert.assertFalse("Expected failure but got success", actualResult);
-        }
-
-        Assert.assertTrue("Expected message to contain: " + expectedMessage, actualMessage.contains(expectedMessage));
-    }
-}
-*/
 package stepDefinitions;
 
 import java.time.Duration;
@@ -253,13 +150,7 @@ public class LibraryServiceSteps {
         page.clickChatSubmit(); // Calls the chat submit button
     }
 
-    //@Then("a success message should be displayed for chat")
-    /*public void a_success_message_should_be_displayed_for_chat() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("mediumchatoutput")));
-        WebElement chatsuccessMessage = null;
-		Assert.assertTrue(chatsuccessMessage.isDisplayed(), "Chat success message not displayed");
-    }*/
+   
     @Then("verify the chat result is {string} and the message should be {string}")
     public void verify_the_chat_result_is_and_the_message_should_be(String expectedResult, String expectedMessage) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
